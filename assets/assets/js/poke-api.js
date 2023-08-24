e4a94da573d327c32bdc15c895cf682c0c8916c4
Matheus rabelo <matheusrabelo520@gmail.com>
2023-08-23 23:26:19 -0300

    Manipulando multiplas requisições em paralelo

--- assets/assets/js/poke-api.js
+++ assets/assets/js/poke-api.js
@@ -1,22 +1,18 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json())
+}
+
     pokeApi.getPokemons = (offset = 0, limit = 10) =>  {
      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return  fetch(url)
+    return fetch(url)
      .then((response) => response.json())
      .then((jsonBody) => jsonBody.results)
-    
+     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+     .then((detailRequest) => Promise.all(detailRequest))
+     .then((pokemonsDetails) => pokemonsDetails)
 
     }
 
-    Promise.all([
-       fetch('https://pokeapi.co/api/v2/pokemon/1'),
-       fetch('https://pokeapi.co/api/v2/pokemon/2'),
-       fetch('https://pokeapi.co/api/v2/pokemon/3'),
-       fetch('https://pokeapi.co/api/v2/pokemon/4'),
-       fetch('https://pokeapi.co/api/v2/pokemon/5')
-       
-    ]).then((results) => {
-        console.log(result);
-    })
