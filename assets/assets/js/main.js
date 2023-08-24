c67cd2cb49e1c301551fbc634893397e1bb53085
Matheus rabelo <matheusrabelo520@gmail.com>
2023-08-23 21:46:05 -0300

    Manipulando o resultado da requisiççao através de uma promise

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -2,8 +2,8 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response){
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error())
 
-console.log(response)
-
-})
