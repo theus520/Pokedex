fb0d879c2ac48921b1ecc140ecc4f56cd61008a1
Matheus rabelo <matheusrabelo520@gmail.com>
2023-08-23 22:35:59 -0300

    Separando o consumo da API da manipulação de HTML

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToLi(pokemon){
     return `  <li class="pokemon">
@@ -19,20 +17,18 @@ function convertPokemonToLi(pokemon){
 </li>`
 }
 
+
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
 
     for (let i = 0; i< pokemons.length; i++) {
-   const pokemon = pokemons[i];
-   pokemonList.innerHTML += convertPokemonToLi(pokemon)
- 
+     const pokemon = pokemons[i];
+     listItems.push(convertPokemonToLi(pokemon))
 }
-})
-.catch((error) => console.error(error)) 
+console.log(listItems);
+ 
 
+})
 
