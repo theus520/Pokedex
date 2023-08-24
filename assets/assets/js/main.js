42f9dbc5652571abad975e8c171b3caed40cc71e
Matheus rabelo <matheusrabelo520@gmail.com>
2023-08-23 22:47:55 -0300

    Utilizando a função Map para diminuir a verbosidade do código

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -17,18 +17,11 @@ function convertPokemonToLi(pokemon){
 </li>`
 }
 
-
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
+pokeApi.getPokemons().then((pokemons = [] ) => {
 
-    for (let i = 0; i< pokemons.length; i++) {
-     const pokemon = pokemons[i];
-     listItems.push(convertPokemonToLi(pokemon))
-}
-console.log(listItems);
- 
+pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
 
 })
 
