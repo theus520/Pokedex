9f9ba8621a60573704b87fb42dab9105eda791c9
Matheus rabelo <matheusrabelo520@gmail.com>
2023-08-24 21:38:30 -0300

    Projeto finalizado com sucesso

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
 const limit = 10
-let offset = 0;
+let offset = 0;  
 
 function convertPokemonToLi(pokemon) {
     return `
